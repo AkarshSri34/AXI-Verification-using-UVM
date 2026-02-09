dd403e18ae7f319efe716ec5d7c44181c70d6b18
Akarsh Srivastava <88154273+AkarshSri34@users.noreply.github.com>
2026-02-09 16:16:12 +0530

    Create sequencer.sv

---
+++ DUT/sequencer.sv
@@ -0,0 +1,8 @@
+class axi_sequencer extends uvm_sequencer#(axi_write_txn);
+  `uvm_component_utils(axi_sequencer)
+  
+  function new(string name,uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+endclass
