7b44a01812a9328c0b5a210fbee4c14c84e75dfd
Akarsh Srivastava <88154273+AkarshSri34@users.noreply.github.com>
2026-02-10 14:50:15 +0530

    Update driver.sv

--- DUT/driver.sv
+++ DUT/driver.sv
@@ -1,4 +1,4 @@
-class axi_driver extends uvm_driver #(axi_write_txn);
+class axi_driver extends uvm_driver #(axi_base_txn);
   `uvm_component_utils(axi_driver)
 
   virtual axi_if vif;
@@ -12,24 +12,47 @@ class axi_driver extends uvm_driver #(axi_write_txn);
   endfunction
 
   task run_phase(uvm_phase phase);
-    axi_write_txn tx;
-    vif.AWVALID <= 0;
-    vif.WVALID  <= 0;
-    vif.WLAST   <= 0;
-    vif.BREADY  <= 0;
-    vif.WSTRB   <= '0;
-    vif.WDATA   <= '0;
-    wait (vif.ARESETn);   // wait for reset release
-  @(posedge vif.ACLK);  // align to clock
     
-    forever begin
-      seq_item_port.get_next_item(tx);
-      drive_aw(tx);
-      drive_w(tx);
-      drive_b(tx);
-      seq_item_port.item_done();
+    axi_write_txn wtx;
+    axi_read_txn  rtx;
+    axi_base_txn  btx;
+
+
+  // Init signals
+  vif.AWVALID <= 0;
+  vif.WVALID  <= 0;
+  vif.WLAST   <= 0;
+  vif.BREADY  <= 0;
+  vif.ARVALID <= 0;
+  vif.RREADY  <= 0;
+
+  wait(vif.ARESETn);
+  @(posedge vif.ACLK);
+
+  forever begin
+    seq_item_port.get_next_item(btx);
+
+    // WRITE transaction
+    if ($cast(wtx, btx)) begin
+      drive_aw(wtx);
+      drive_w(wtx);
+      drive_b(wtx);
     end
-  endtask
+
+    // READ transaction
+    else if ($cast(rtx, btx)) begin
+      drive_ar(rtx);
+      drive_r(rtx);
+    end
+
+    else begin
+      `uvm_fatal("DRV", "Unknown AXI transaction type")
+    end
+
+    seq_item_port.item_done();
+  end
+endtask
+
 
   task drive_aw(axi_write_txn tx);
     @(posedge vif.ACLK);
@@ -38,6 +61,7 @@ class axi_driver extends uvm_driver #(axi_write_txn);
     vif.AWLEN   <= tx.awlen;
     vif.AWSIZE  <= tx.awsize;
     vif.AWBURST <= tx.awburst;
+    vif.AWID    <= tx.awid;
 
     do @(posedge vif.ACLK); while (!vif.AWREADY);
     vif.AWVALID <= 0;
@@ -56,12 +80,51 @@ class axi_driver extends uvm_driver #(axi_write_txn);
       vif.WLAST  <= 0;
     end
   endtask
+  
+  task drive_ar(axi_read_txn tx);
+  @(posedge vif.ACLK);
+  vif.ARVALID <= 1;
+  vif.ARID    <= tx.arid; 
+  vif.ARADDR  <= tx.araddr;
+  vif.ARLEN   <= tx.arlen;
+  vif.ARSIZE  <= tx.arsize;
+  vif.ARBURST <= tx.arburst;
+
+  do @(posedge vif.ACLK); while (!vif.ARREADY);
+  vif.ARVALID <= 0;
+endtask
+  
+  
+task drive_r(axi_read_txn tx);
+  tx.rdata = new[tx.arlen + 1];
+  tx.rresp = new[tx.arlen + 1];
+
+  for (int i = 0; i <= tx.arlen; i++) begin
+    @(posedge vif.ACLK);
+    vif.RREADY <= 1;
+
+    do @(posedge vif.ACLK); while (!vif.RVALID);
+
+    tx.rdata[i] = vif.RDATA;
+    tx.rid[i]   = vif.RID;   
+    tx.rresp[i] = vif.RRESP;
+
+    if (vif.RLAST)
+      break;
+  end
+
+  vif.RREADY <= 0;
+endtask
+
 
   task drive_b(axi_write_txn tx);
     @(posedge vif.ACLK);
     vif.BREADY <= 1;
     do @(posedge vif.ACLK); while (!vif.BVALID);
     tx.bresp = vif.BRESP;
+    tx.bid   = vif.BID;
     vif.BREADY <= 0;
   endtask
 endclass
+
+
