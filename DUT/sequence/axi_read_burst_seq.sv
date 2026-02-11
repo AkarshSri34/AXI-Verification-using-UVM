17d5ef89f79a6941e053a1613d6fb2826b4f9f26
Akarsh Srivastava <88154273+AkarshSri34@users.noreply.github.com>
2026-02-11 16:20:41 +0530

    Refactor axi_read_burst_seq to use rtx variable

--- DUT/sequence/axi_read_burst_seq.sv
+++ DUT/sequence/axi_read_burst_seq.sv
@@ -1,21 +1,30 @@
 class axi_read_burst_seq extends uvm_sequence #(axi_read_txn);
   `uvm_object_utils(axi_read_burst_seq)
 
+  bit [31:0] start_addr;
+  bit [7:0]  start_len;
+  bit [2:0]  start_size;
+  bit [1:0]  start_burst;
+
+  
   function new(string name="axi_read_burst_seq");
     super.new(name);
   endfunction
 
   task body();
-    axi_read_txn tx;
+     axi_read_txn rtx;
+    repeat (1) begin
+      rtx = axi_read_txn::type_id::create("tx");
 
-    repeat (3) begin
-      tx = axi_read_txn::type_id::create("tx");
-
-      start_item(tx);
-      if (!tx.randomize()) begin
+      start_item(rtx);
+      if (!rtx.randomize() with { araddr == start_addr;
+                                  arlen   == start_len;
+                                  arsize  == start_size;
+                                  arburst == start_burst;})
+        begin
         `uvm_error("READ_SEQ", "Randomization failed")
       end
-      finish_item(tx);
+      finish_item(rtx);
     end
   endtask
 
